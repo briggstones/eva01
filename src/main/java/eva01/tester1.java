aa30ce6edd58c23ae76c0b877358765887ccecac
briggstones <rodrigo.benitez.g@gmail.com>
2024-10-14 21:33:23 -0300

    mi primer commit

---
+++ src/main/java/eva01/tester1.java
@@ -0,0 +1,13 @@
+package eva01;
+
+public class tester1 {
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			System.out.printf("hola mundo!!!!!!!!!!!!!!");
+		}
+
+	}
+
+	
+
